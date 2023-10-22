6318ef3fbd20824f8d62d2e7ad9a2d249c9047d5
Nikoloz Gachechiladze <nikolozgachechiladze@Nikolozs-MacBook-Air.local>
2023-10-23 01:32:58 +0400

    tweaked some errors

--- USJ/GCD-LCM/GCD-LCM/ViewController.swift
+++ USJ/GCD-LCM/GCD-LCM/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Nikoloz Gachechiladze on 22.10.23.
 //
-// Tested on iPhone 14 Pro
+//  Tested on iPhone 14 Pro
 
 import UIKit
 
@@ -37,34 +37,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button(_ sender: Any) {
-           if switchButton.isOn {
-               if let textfieldInt = Int(field1.text ?? ""), let textfield2Int = Int(field2.text ?? "") {
-                   let gcd = calculateGCD(a: textfieldInt, b: textfield2Int)
-                   calculatedNum.text = String(gcd)
-                   field1.textColor = UIColor.black
-                   field2.textColor = UIColor.black
-               }
-               else
-               {
-                   calculatedNum.text = "Invalid input"
-                   field1.textColor = UIColor.red
-                   field2.textColor = UIColor.red
-               }
+        if switchButton.isOn {
+            if let textfieldInt = Int(field1.text ?? ""), let textfield2Int = Int(field2.text ?? "") {
+                let gcd = calculateGCD(a: textfieldInt, b: textfield2Int)
+                calculatedNum.text = String(gcd)
+                field1.textColor = UIColor.black
+                field2.textColor = UIColor.black
             }
-             else
-             {
-               if let textfieldInt = Int(field1.text ?? ""), let textfield2Int = Int(field2.text ?? "") {
-                   let lcm = calculateLCM(a: textfieldInt, b: textfield2Int)
-                   calculatedNum.text = String(lcm)
-                   field1.textColor = UIColor.black
-                   field2.textColor = UIColor.black
-               } else {
-                   calculatedNum.text = "Invalid input"
-                   field1.textColor = UIColor.red
-                   field2.textColor = UIColor.red
-               }
-           }
-       }
+            else
+            {
+                calculatedNum.text = "Invalid input"
+                field1.textColor = UIColor.red
+                field2.textColor = UIColor.red
+            }
+        }
+        else
+        {
+            if let textfieldInt = Int(field1.text ?? ""), let textfield2Int = Int(field2.text ?? "") {
+                let lcm = calculateLCM(a: textfieldInt, b: textfield2Int)
+                calculatedNum.text = String(lcm)
+                field1.textColor = UIColor.black
+                field2.textColor = UIColor.black
+            }
+            else
+            {
+                calculatedNum.text = "Invalid input"
+                field1.textColor = UIColor.red
+                field2.textColor = UIColor.red
+            }
+        }
+    }
     
     
 }
